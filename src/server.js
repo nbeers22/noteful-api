e5596c1b508ccd4e4ae9b7474d87f57119e6a5c6
nbeers22 <nbeers22@gmail.com>
2019-07-11 12:08:34 -0400

    Set knex db instance as default

--- src/server.js
+++ src/server.js
@@ -1,5 +1,12 @@
 const app = require('./app.js');
-const { PORT } = require('./config.js');
+const { PORT, DB_URL } = require('./config.js');
+
+const db = knex({
+  client: 'pg',
+  connection: DB_URL
+});
+
+app.set('db',db);
 
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
